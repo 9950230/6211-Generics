14d437300beda22840d2d43d6feeb544a0a9b9ae
Brett Griffin <9950230@student.toiohomai.ac.nz>
2019-04-05 14:23:08 +1300

    First commit

---
+++ GenericClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GenericClass<int> integerGeneric = new GenericClass<int>(1);
+            GenericClass<string> stringGeneric = new GenericClass<string>("string");
+            GenericClass<bool> boolGeneric = new GenericClass<bool>(true);
+
+            Console.ReadLine();
+
+        }
+    }
+    public interface IGenericClass<T>
+    {
+        void Write(T anytype);
+    }
+
+    class GenericClass<T> : IGenericClass<T>
+    {
+        public GenericClass(T anytype)
+        {
+            Write(anytype);
+        }
+
+        public void Write(T anytype)
+        {
+            Console.WriteLine(anytype.ToString());
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Input an integer: ");
+                int integer = 0;
+                try
+                {
+                    integer = Int32.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                Console.Write("Input a string: ");
+                string string1 = Console.ReadLine();
+
+                if (Compare<int, string>(integer, string1))
+                    Console.WriteLine("The integer and the string are equal");
+                else
+                    Console.WriteLine("The integer and the string are NOT equal");
+
+                Console.WriteLine();
+
+            }
+        }
+
+        static bool Compare<I, X>(I i, X x)
+        {
+            return i.ToString() == x.ToString() ? true : false;
+        }
+
+
+
+    }
+}
